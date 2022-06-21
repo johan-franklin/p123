8f8cd3378337776ba63105bc45e80d44620b7c8a
johan-franklin <87813497+johan-franklin@users.noreply.github.com>
2022-06-21 18:53:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
+function setup() {
+  canvas = createCanvas(450, 400);
+  canvas.position(560, 150);
+
+    video = createCapture(VIDEO);
+    video.size(450, 400);
+   
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+  }
+
+  function modelLoaded() {
+    console.log('PoseNet Is Initialized!');
+  }
+  
+  
+  function gotPoses(results)
+  {
+    if(results.length > 0)
+    {
+      console.log(results);
+      
+      leftWristX = results[0].pose.leftWrist.x;
+      rightWristX = results[0].pose.rightWrist.x;
+      difference = floor(leftWristX - rightWristX);
+    }
+  }
+
+  function draw() {
+
+  background('#cc6699');
+
+  
+
+textSize(difference);
+fill('#3333ff');
+text('Johan', 50, 400);
+  }
+  
